3ee5adfeb2fa5e1effdbab0c8d9f0f13c72d79cb
Hafizuddin <mattot.builder@gmail.com>
2024-08-15 13:47:00 +0800

    Updated which key spec
    register -> add

--- lua/mattot/plugins/flutter-tools.lua
+++ lua/mattot/plugins/flutter-tools.lua
@@ -6,4 +6,11 @@ return {
     'stevearc/dressing.nvim', -- optional for vim.ui.select
   },
   config = true,
+  opts = {
+    vim.keymap.set('n', '<leader>FR', '<cmd>FlutterRun<cr>', { desc = '[F]lutter [R]un' }),
+    vim.keymap.set('n', '<leader>Frl', '<cmd>FlutterReload<cr>', { desc = '[F]lutter [R]e[L]oad' }),
+    vim.keymap.set('n', '<leader>Frs', '<cmd>FlutterRestart<cr>', { desc = '[F]lutter [R]e[S]tart' }),
+    vim.keymap.set('n', '<leader>Fo', '<cmd>FlutterOutlineToggle<cr>', { desc = '[F]lutter [O]utline toggle' }),
+    vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = '[F]lutter [Q]uit' }),
+  },
 }
